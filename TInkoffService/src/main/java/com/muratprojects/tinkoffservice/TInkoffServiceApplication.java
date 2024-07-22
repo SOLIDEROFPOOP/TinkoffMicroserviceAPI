9cf8e46dbed1fbb5022a199c277aa5cd7551c7a7
Kapparov Murat <paprim2003@gmail.com>
2024-07-22 14:40:20 +0500

    project init

---
+++ TInkoffService/src/main/java/com/muratprojects/tinkoffservice/TInkoffServiceApplication.java
@@ -0,0 +1,13 @@
+package com.muratprojects.tinkoffservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TInkoffServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TInkoffServiceApplication.class, args);
+    }
+
+}
---
+++ TInkoffService/src/test/java/com/muratprojects/tinkoffservice/TInkoffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratprojects.tinkoffservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TInkoffServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
